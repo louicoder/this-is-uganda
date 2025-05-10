a1609deaee62f97ced5303c68f017d74d2cf5044
Musanje Louis Michael <32771081+louicoder@users.noreply.github.com>
2025-05-10 22:45:02 +0300

    feat: add aliases to streamline imports

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
     // Everyhting should be above this.
     'react-native-reanimated/plugin',
   ],
