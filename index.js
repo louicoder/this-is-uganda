8bc5cf88515c99a3da5bcdf6decc61d588cda6ad
Musanje Louis Michael <32771081+louicoder@users.noreply.github.com>
2025-05-11 17:10:21 +0300

    feat: add stacks, organise navigation files, add navigation example

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+import App from '@/navigation/index';
 import {name as appName} from './app.json';
 // import { Toaster } from 'sonner-native';
 // import { SafeAreaProvider as Safe } from 'react-native-safe-area-context';
